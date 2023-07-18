deca8bc9c512ddad1b16001fe939796ab3369287
Imad <imadzaanine@gmail.com>
2023-07-18 18:05:27 +0200

    the childs cahnges color on hover

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-function NewDiv(container,gridValue) {
-    for (let i=gridValue; i > 0; i--) {
-        for (let j = gridValue; j > 0; j--) {
-            const div = document.createElement('div');
-            container.appendChild(div);
-        }
-        
+function NewDiv(container, gridValue) {
+  for (let i = gridValue; i > 0; i--) {
+    for (let j = gridValue; j > 0; j--) {
+      const div = document.createElement("div");
+      div.setAttribute("id", "Sketch");
+      container.appendChild(div);
     }
+  }
 }
+let color = "black";
+function ChangeBackgroundColor(event) {
+    const div = event.target;
+    div.style.backgroundColor = color;
+  }
+const container = document.querySelector(".container");
+NewDiv(container, 16);
+const Sketch = document.querySelectorAll("#Sketch");
+Sketch.forEach(div => {
+  div.addEventListener("mouseenter", ChangeBackgroundColor);
+});
