7758e435c85722742f8a24881ede533ad247f94b
Imad <imadzaanine@gmail.com>
2023-07-18 18:09:38 +0200

    Fixed a bug

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ function NewDiv(container, gridValue) {
   for (let i = gridValue; i > 0; i--) {
     for (let j = gridValue; j > 0; j--) {
       const div = document.createElement("div");
-      div.style.gridTemplateColumns = `repeat(${gridValue}, auto)`;
-      div.style.gridTemplateRows = `repeat(${gridValue}, auto)`;
+      container.style.gridTemplateColumns = `repeat(${gridValue}, auto)`;
+      container.style.gridTemplateRows = `repeat(${gridValue}, auto)`;
       div.setAttribute("id", "Sketch");
       container.appendChild(div);
     }
